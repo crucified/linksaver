3851fb3c6df90a3780f4d7cf5601f06a89150e6a
Your Name <kharitonov.dennis@gmail.com>
2012-04-22 20:12:46 +0400

    Minor improvements and addons. 2DO: bug - can make user with no password. Fix by validators

--- db/migrate/20120322155813_create_contents.rb
+++ db/migrate/20120322155813_create_contents.rb
@@ -2,7 +2,7 @@ class CreateContents < ActiveRecord::Migration
   def change
     create_table :contents do |t|
       t.string "title", :default => ""
-      t.string "link"
+      t.string "link", :default => ""
       t.datetime "created_at"
       t.timestamps
     end
